76b06bdd91828c38be40e27235c1bd37d32536f4
Nicxv <nico0valenzuelacl@gmail.com>
2023-05-21 22:59:59 -0400

    Modificaciones

--- menu/urls.py
+++ menu/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import principal,samsung,xiaomi,home,index, contacto, borgona, borgona2
+from .views import principal, samsung, xiaomi, home, index, contacto, borgona, borgona2
 
 urlpatterns = [
-    path('',principal,name="princial")
-    path('samsung',samsung,name="samsung")
-    path('xiaomi',xiaomi,name="xiaomi")
+    path('',principal,name="princial"),
+    path('samsung',samsung,name="samsung"),
+    path('xiaomi',xiaomi,name="xiaomi"),
     path('index',index,name="index"),
     path('home',home,name="home"),
     path('contacto',contacto,name="contacto"),
