bc28918386deb159b19af10bebb998158363ff59
Sim0n2o03 <si.caroca@duocuc.cl>
2023-05-23 23:46:19 -0400

    cambios

--- menu/urls.py
+++ menu/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import principal, samsung, xiaomi, Xiaomi Redmi Note 12, Samsung Galaxy S10 home, registro, cambiarcontra, index, contacto, borgona, borgona2
+from .views import principal, samsung, xiaomi, Xiaomi_redmi_note_12, Samsung_galaxy_s10, registro, cambiarcontra, index,home, contacto, borgona, borgona2
 
 urlpatterns = [
     path('',principal,name="principal"),
     path('samsung',samsung,name="samsung"),
     path('micuenta',xiaomi,name="micuenta"),
-    path('Xiaomi Redmi Note 12',Xiaomi Redmi Note 12,name="Xiaomi Redmi Note 12"),
-    path('Samsung Galaxy S10',Samsung Galaxy S10,name="Samsung Galaxy S10"),
+    path(' Xiaomi_redmi_note_12', Xiaomi_redmi_note_12,name=" Xiaomi_redmi_note_12"),
+    path('Samsung_galaxy_s10',Samsung_galaxy_s10,name="Samsung_galaxy_s10"),
     path('cambiarcontra',cambiarcontra,name="cambiarcontra"),
     path('registro',registro,name="registro"),
 
--- menu/views.py
+++ menu/views.py
@@ -13,11 +13,11 @@ def xiaomi(request):
 def micuenta(request):
     return render(request,'menu/micuenta.html')
 
-def Samsung Galaxy S10(request):
-    return render(request,'menu/Samsung Galaxy S10.html')
+def Samsung_galaxy_s10(request):
+    return render(request,'menu/Samsung_galaxy_s10.html')
 
-def Xiaomi Redmi Note 12(request):
-    return render(request,'menu/Xiaomi Redmi Note 12.html')   
+def Xiaomi_redmi_note_12(request):
+    return render(request,'menu/Xiaomi_redmi_note_12.html')   
 
 def cambiarcontra(request):
     return render(request,'menu/cambiarcontra.html')
